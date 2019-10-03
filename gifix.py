75520792914b624cdd985e84194acb288f8a7f80
yongsk0066 <yongsk0066@naver.com>
2019-10-03 14:52:33 +0900

    create application extension dict return func

--- gifix.py
+++ gifix.py
@@ -86,9 +86,6 @@ with open(file, 'rb+') as f:
 # print(wrap(q[26:1562], 6))
 # print(256* 3 * 2 + 26)
 # print(wrap(q[1562:1600], 2))
-# print(wrap(q[1600:1616], 2))
-# print(wrap(q[1616:1680], 2))
-# print(type(bin(int('0xFF', 16))))
 #
 ####################################################
 
@@ -119,9 +116,44 @@ def header_info(q):
     header_dict['PAR'] = q[24:26]
 
     # Global Color Table
+    # 26 ~ size of Global Color Table
     GCT = wrap(q[26:26 + header_dict['GCT_size'] * 2], 6)
     header_dict['GCT'] = GCT
+
+    header_dict['last_digit'] = 26 + header_dict['GCT_size'] * 2
     return header_dict
 
 
-print(header_info(q))
+h_d = header_info(q)
+l_d = h_d['last_digit']
+
+
+# print(h_d['last_digit'])
+# print(wrap(q[h_d['last_digit']:1568], 2))
+# print(wrap(q[1568:1590], 2))
+# print(''.join([chr(int('0x' + x, 16)) for x in wrap(q[1568:1590], 2)]))
+# print(q[1590:1592])
+# print(q[1592:1594])
+# print(q[1594:1598])
+# print(q[1598:1600])
+
+
+def application_extension(q, d):
+    dt = {}
+    dt['Introducer'] = 21
+    dt['Label'] = 'ff'
+    dt['Block_size_f'] = 11
+    dt['App_data'] = ''.join([chr(int('0x' + x, 16)) for x in wrap(q[6:6 + dt['Block_size_f'] * 2], 2)])
+    dt['Block_size_s'] = 3
+    dt['Extension_type'] = int('0x' + q[30:32], 16)
+    dt['Repeat_count'] = h2h_int(q[32:36])
+    dt['Terminator'] = q[36:38]
+    dt['last_digit'] = d + 38
+
+    return dt
+
+
+print(h_d)
+print(application_extension(q[l_d:l_d + 38], l_d))
+print(wrap(q[1600:1616], 2))
+print(wrap(q[1616:1680], 2))
